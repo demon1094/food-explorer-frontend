80f8d5078de03148a05b7c07170476564fd044fa
Diego Araujo <diegoagar345@gmail.com>
2023-06-16 11:52:41 -0300

    feat: add responsiviness to page Orders

--- src/pages/Orders/index.jsx
+++ src/pages/Orders/index.jsx
@@ -1,22 +1,39 @@
-import { Container, OrdersWrapper } from "./styles"
+import { Container, OrdersWrapperMobile, OrdersWrapperDesktop } from "./styles"
 
 import { EmptyDisplay } from "../../components/EmptyDisplay"
 import { Header } from "../../components/Header"
 import { Footer } from "../../components/Footer"
-import { Order } from "../../components/Order"
+import { Order, Status } from "../../components/Order"
 
 import { TfiFaceSad } from "react-icons/tfi"
 
-import { ToastContainer } from "react-toastify"
+import { ToastContainer, toast } from "react-toastify"
+import { toastConfig } from "../../configs/toastConfig"
+import "react-toastify/dist/ReactToastify.css"
 
 import { useState, useEffect } from "react"
+import { useAuth } from "../../hooks/auth"
 
 import { api } from "../../services/api"
 
 export function Orders() {
   const [ orders, setOrders ] = useState([])
 
-  console.log(orders)
+  const { user } = useAuth()
+
+  async function handleUpdateStatus(order_id, newStatus) {
+    await api.patch(`/orders?id=${order_id}&status=${newStatus}`)
+    .then(() => {
+      toast.success('Status do pedido atualizado.', toastConfig)
+    })
+    .catch((error) => {
+      if (error.response) {
+        toast.error(error.response.data.message, toastConfig)
+      } else {
+        toast.error('Erro ao atualizar o pedido.', toastConfig)
+      }
+    })
+  }
 
   useEffect(() => {
     async function fetchOrders() {
@@ -40,8 +57,9 @@ export function Orders() {
       <Header />
 
       <main>
-        <OrdersWrapper>
-          <h1>Pedidos</h1>
+        <h1>Pedidos</h1>
+
+        <OrdersWrapperMobile>
 
           {
             orders.length > 0 &&
@@ -66,7 +84,45 @@ export function Orders() {
               message="Nenhum pedido encontrado"
             />
           }
-        </OrdersWrapper>
+        </OrdersWrapperMobile>
+
+        <OrdersWrapperDesktop>
+          <header>
+            <div>Status</div>
+            <div>Código</div>
+            <div>Detalhamento</div>
+            <div>Data e Hora</div>
+          </header>
+
+          <div className="body">
+            {
+              orders.length > 0 &&
+              orders.map((order) => (
+                <>
+                  <div className="order-row">
+                    {
+                      !user.isAdmin ?
+                      <Status status={order.status} />
+                      :
+                      <select
+                        name="status"
+                        id="status"
+                        onChange={(e) => handleUpdateStatus(order.id, e.target.value)}
+                      >
+                        <option value="pending" selected={order.status === 'peding'}>Pendente</option>
+                        <option value="preparing" selected={order.status === 'preparing'}>Preparando</option>
+                        <option value="delivered" selected={order.status === 'delivered'}>Entregue</option>
+                      </select>
+                    }
+                  </div>
+                  <div className="order-row">{String(order.id).padStart(5, '00000')}</div>
+                  <div className="order-row">{order.description}</div>
+                  <div className="order-row">{order.created_at}</div>
+                </>
+              ))
+            }
+          </div>
+        </OrdersWrapperDesktop>
       </main>
 
       <Footer />
--- src/pages/Orders/styles.js
+++ src/pages/Orders/styles.js
@@ -3,25 +3,30 @@ import styled from "styled-components"
 export const Container = styled.div`
   width: 100%;
   height: 100vh;
-
+  
   > main {
     width: 100%;
     min-height: 100%;
+    margin: 0 auto;
     padding: 5rem 3rem;
     background: ${ ({ theme }) => theme.COLORS.BODY_BG };
+
+    h1 {
+      font-family: 'Poppins', sans-serif;
+      margin-bottom: 3rem;
+      font-size: 3.2rem;
+      font-weight: 500;
+      color: ${ ({ theme }) => theme.COLORS.WHITE_TEXT };
+    }
+
+    @media screen and (min-width: 1024px) {
+      padding: 8rem 10rem;
+    }
   }
 `
 
-export const OrdersWrapper = styled.section`
+export const OrdersWrapperMobile = styled.section`
   width: 100%;
-
-  > h1 {
-    font-family: 'Poppins', sans-serif;
-    margin-bottom: 3rem;
-    font-size: 3.2rem;
-    font-weight: 500;
-    color: ${ ({ theme }) => theme.COLORS.WHITE_TEXT };
-  }
 
   > div:nth-child(2) {
     width: 100%;
@@ -29,5 +34,73 @@ export const OrdersWrapper = styled.section`
     flex-direction: column;
     align-items: center;
     gap: 3rem;
+  }
+
+  @media screen and (min-width: 1024px) {
+    display: none;
+  }
+`
+
+export const OrdersWrapperDesktop = styled.div`
+  width: 100%;
+  height: 100%;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 8rem 1fr;
+  grid-template-areas:
+  'header'
+  'body';
+  border-radius: 10px 10px 0 0;
+  border: 1px solid ${ ({ theme }) => theme.COLORS.GRAY_200 };
+  font-family: 'Poppins', sans-serif;
+  color: ${ ({ theme }) => theme.COLORS.WHITE_TEXT };
+
+  > header {
+    grid-area: header;
+    display: grid;
+    grid-template-columns: 1fr 1fr 3fr 1fr;
+
+    div {
+      padding: 2rem;
+      display: flex;
+      align-items: center;
+      border: 1px solid ${ ({ theme }) => theme.COLORS.GRAY_200 };
+
+      &:first-child {
+        border-radius: 10px 0 0 0;
+      }
+
+      &:last-child {
+        border-radius: 0 10px 0 0;
+      }
+    }
+  }
+
+  > .body {
+    grid-area: body;
+    display: grid;
+    grid-template-columns: 1fr 1fr 3fr 1fr;
+
+    .order-row {
+      padding: 2rem;
+      display: flex;
+      align-items: center;
+      border: 1px solid ${ ({ theme }) => theme.COLORS.GRAY_200 };
+
+      select {
+        appearance: none;
+        width: 100%;
+        height: 5rem;
+        margin: 0;
+        padding: 1rem 2rem;
+        border: none;
+        border-radius: .5rem;
+        font-family: 'Poppins', sans-serif;
+        font-size: 1.5rem;
+        cursor: inherit;
+        background: ${ ({ theme }) => theme.COLORS.INPUT_BG };
+        color: ${ ({ theme }) => theme.COLORS.WHITE_TEXT_300 };
+      }
+    }
   }
 `
